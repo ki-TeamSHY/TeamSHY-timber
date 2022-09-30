75fe4f8a7d3ced0e794d73b555a071236da01c18
Librarian423 <youg4231998@naver.com>
2022-09-30 10:45:02 +0900

    Revert "SceneManager 추가"
    This reverts commit 8700e81609b82ddc5a4c03bb5a927fc01f0ae9b3.

--- timber/SceneManager.h
+++ timber/SceneManager.h
@@ -1,19 +1,5 @@
 #pragma once
-#include <iostream>
-#include "InputManager.h"
-#include "Utils.h"
-
-using namespace std;
-using namespace sf;
-
 class SceneManager
 {
-private:
-	int pos;
-
-public:
-	SceneManager();
-	~SceneManager();
-	void SelectGameMode();
 };
 
